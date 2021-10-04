69f916e8dfed695f60863ee07da70f0f594c38c9
Smart97 <Smart9713@gmail.com>
2021-10-04 15:39:32 +0300

    oop change

--- l1/script.js
+++ l1/script.js
@@ -1,4 +1,4 @@
-const goods = [
+/* const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
@@ -21,3 +21,39 @@ const renderGoodsList = (list = goods) => {
 }
   
 renderGoodsList();
+ */
+
+class goodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`
+    }
+}
+class goodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+    render(container) {
+        let listHTML = '';
+        this.goods.forEach(good => {
+            const goodItem = new goodsItem(good.title, good.price);
+            listHTML += goodItem.render();
+            console.log(good)
+        }); 
+         document.querySelector(container).insertAdjacentHTML('beforeend', listHTML)    
+    }
+}
+const list = new goodsList();
+list.fetchGoods();
+list.render('.goods-list');
